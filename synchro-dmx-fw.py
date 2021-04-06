65da8988cf38034b62653bfbd55a67cd75b3fcd7
LaurentRavera <laurent.ravera@irap.omp.eu>
2021-04-06 17:06:33 +0200

    Changes record: - synchro-dmx-fw updated tu use C_CLK_REF_CLK=60MHz

--- synchro-dmx-fw.py
+++ synchro-dmx-fw.py
@@ -3,16 +3,20 @@ svg = wavedrom.render("""
 {
 "signal" : 
  [
-  { "name": "CLK_REF  (20 MHz)", "wave": "h..l....h....l....h....l....h....l....h....l....", "period": 1 },
-  { "name": "SYNC", "wave": "l.......h.........l.............................", "node": "..........a" ,"period": 1 },
+  { "name": "CLK_REF  (60 MHz)", "wave": "P.............", "period": 2 },
+  { "name": "SYNC", "wave": "l...h.l.....................", "node": ".....a" ,"period": 1 },
   {},
-  { "name": "CLK_ADC (100 MHz)", "wave": "P.......................", "node": ".....b", "period": 2 },
-  { "name": "PIX", "wave": "=.....=.......=.......=.", "data": ["34", "1", "2", "3"], "node": "......c", "period": 2, "phase": 0 }
+  { "name": "CLK_ADC (120 MHz)", "wave": "P...........................", "node": ".....b", "period": 1 },
+  { "name": "PIX (6.66 MHz)", "wave": "=.....=..................=..", "data": ["34", "1", "2"], "node": "......c", "period": 1, "phase": 0 }
  ],
 "edge":
  [
  "a~>c", "b~>c"
  ],
+ "foot":
+ {
+ "tick": 0
+ },
 "config" : { "hscale" : 0.5, "skin": "narrow" }
 } 
 """)
