10d5de00f52f0643f49f61e8ac5a50e45e51334d
LaurentRavera <laurent.ravera@irap.omp.eu>
2022-03-04 14:00:14 +0100

    Interface divided in 2 links

--- if-demux-ep.py
+++ if-demux-ep.py
@@ -1,29 +1,38 @@
 import wavedrom
 svg = wavedrom.render("""
 { "signal" : 
- [
-  { "name": "CTRL", "wave": "01...=.0.=.0.=.0...1..", "data": ["CTRLN [2]", "CTRLN [1]", "CTRLN [0]"] }, 
-   ["DATA",
-   	["Col 0",
-	 { "name": " ", "wave": "x3.3.3.3.3.3.3.3.x.3.3", "data": ["PixN [15]", "PixN [14]", "PixN [13]", "PixN [12]", "PixN [11]", "PixN [10]", "PixN [9]", "PixN [8]", "PixN+1 [15]"] },
-	 { "name": " ", "wave": "x3.3.3.3.3.3.3.3.x.3.3", "data": ["PixN [7]", "PixN [6]", "PixN [5]", "PixN [4]", "PixN [3]", "PixN [2]", "PixN [1]", "PixN [0]", "PixN+1 [7]"] }
-    ],
-   	["Col 1",
-	 { "name": " ", "wave": "x5.5.5.5.5.5.5.5.x.5.5", "data": ["PixN [15]", "PixN [14]", "PixN [13]", "PixN [12]", "PixN [11]", "PixN [10]", "PixN [9]", "PixN [8]", "PixN+1 [15]"] },
-	 { "name": " ", "wave": "x5.5.5.5.5.5.5.5.x.5.5", "data": ["PixN [7]", "PixN [6]", "PixN [5]", "PixN [4]", "PixN [3]", "PixN [2]", "PixN [1]", "PixN [0]", "PixN+1 [7]"] }
-    ],
-   	["Col 2",
-	 { "name": " ", "wave": "x4.4.4.4.4.4.4.4.x.4.4", "data": ["PixN [15]", "PixN [14]", "PixN [13]", "PixN [12]", "PixN [11]", "PixN [10]", "PixN [9]", "PixN [8]", "PixN+1 [15]"] },
-	 { "name": " ", "wave": "x4.4.4.4.4.4.4.4.x.4.4", "data": ["PixN [7]", "PixN [6]", "PixN [5]", "PixN [4]", "PixN [3]", "PixN [2]", "PixN [1]", "PixN [0]", "PixN+1 [7]"] }
-    ],
-   	["Col 3",
-	 { "name": " ", "wave": "x2.2.2.2.2.2.2.2.x.2.2", "data": ["PixN [15]", "PixN [14]", "PixN [13]", "PixN [12]", "PixN [11]", "PixN [10]", "PixN [9]", "PixN [8]", "PixN+1 [15]"] },
-	 { "name": " ", "wave": "x2.2.2.2.2.2.2.2.x.2.2", "data": ["PixN [7]", "PixN [6]", "PixN [5]", "PixN [4]", "PixN [3]", "PixN [2]", "PixN [1]", "PixN [0]", "PixN+1 [7]"] }
-    ]
-  ],
-  { "name": "CLK", "wave": "p..........", "period":2 }
- ],
- "config" : { "hscale" : 1 }
+	[
+		["LINK 1",
+			{ "name": "CLK", "wave": "p..........", "period":2 },
+			{ "name": "CTRL", "wave": "01...=.0.=.0.=.0...1..", "data": ["CTRLN [2]", "CTRLN [1]", "CTRLN [0]"] }, 
+			["DATA",
+				["Col 0",
+					{ "name": " ", "wave": "x3.3.3.3.3.3.3.3.x.3.3", "data": ["Val N [15]", "Val N [14]", "Val N [13]", "Val N [12]", "Val N [11]", "Val N [10]", "Val N [9]", "Val N [8]", "Val N+1 [15]"] },
+					{ "name": " ", "wave": "x3.3.3.3.3.3.3.3.x.3.3", "data": ["Val N [7]", "Val N [6]", "Val N [5]", "Val N [4]", "Val N [3]", "Val N [2]", "Val N [1]", "Val N [0]", "Val N+1 [7]"] }
+				],
+				["Col 1",
+					{ "name": " ", "wave": "x5.5.5.5.5.5.5.5.x.5.5", "data": ["Val N [15]", "Val N [14]", "Val N [13]", "Val N [12]", "Val N [11]", "Val N [10]", "Val N [9]", "Val N [8]", "Val N+1 [15]"] },
+					{ "name": " ", "wave": "x5.5.5.5.5.5.5.5.x.5.5", "data": ["Val N [7]", "Val N [6]", "Val N [5]", "Val N [4]", "Val N [3]", "Val N [2]", "Val N [1]", "Val N [0]", "Val N+1 [7]"] }
+				]
+			]
+		],
+
+		["LINK 2",
+			{ "name": "CLK", "wave": "p..........", "period":2 },
+			{ "name": "CTRL", "wave": "01...=.0.=.0.=.0...1..", "data": ["CTRLN [2]", "CTRLN [1]", "CTRLN [0]"] }, 
+			["DATA",
+				["Col 2",
+					{ "name": " ", "wave": "x4.4.4.4.4.4.4.4.x.4.4", "data": ["Val N [15]", "Val N [14]", "Val N [13]", "Val N [12]", "Val N [11]", "Val N [10]", "Val N [9]", "Val N [8]", "Val N+1 [15]"] },
+					{ "name": " ", "wave": "x4.4.4.4.4.4.4.4.x.4.4", "data": ["Val N [7]", "Val N [6]", "Val N [5]", "Val N [4]", "Val N [3]", "Val N [2]", "Val N [1]", "Val N [0]", "Val N+1 [7]"] }
+				],
+				["Col 3",
+					{ "name": " ", "wave": "x2.2.2.2.2.2.2.2.x.2.2", "data": ["Val N [15]", "Val N [14]", "Val N [13]", "Val N [12]", "Val N [11]", "Val N [10]", "Val N [9]", "Val N [8]", "Val N+1 [15]"] },
+					{ "name": " ", "wave": "x2.2.2.2.2.2.2.2.x.2.2", "data": ["Val N [7]", "Val N [6]", "Val N [5]", "Val N [4]", "Val N [3]", "Val N [2]", "Val N [1]", "Val N [0]", "Val N+1 [7]"] }
+				]
+			]
+		]
+	],
+	"config" : { "hscale" : 1 }
 } 
 """)
 svg.saveas("interface-demux-ep.svg")
